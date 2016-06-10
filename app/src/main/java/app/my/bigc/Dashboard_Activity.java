4dbfcb13acc781c7ebea5170051ce399891f58dc
prudhviraj <prudhviraj038@gmail.com>
2016-06-10 12:45:41 +0530

    jk

--- app/src/main/java/app/my/bigc/Dashboard_Activity.java
+++ app/src/main/java/app/my/bigc/Dashboard_Activity.java
@@ -13,12 +13,20 @@ import android.widget.TextView;
  */
 public class Dashboard_Activity extends Activity {
     ImageView employee,customer;
+    LinearLayout home,customerfeedback,missedcustomerfeedback,employeexam,result,notification;;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation_activity);
         employee = (ImageView)findViewById(R.id.employee);
         customer = (ImageView)findViewById(R.id.customer);
+        home = (LinearLayout)findViewById(R.id.home_layout);
+        customerfeedback = (LinearLayout)findViewById(R.id.customer_layout);
+        missedcustomerfeedback = (LinearLayout)findViewById(R.id.missedcustomer_layout);
+        employeexam = (LinearLayout)findViewById(R.id.employeeexam_layout);
+        result = (LinearLayout)findViewById(R.id.result_layout);
+        notification = (LinearLayout)findViewById(R.id.notification_layout);
+
         employee.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,6 +44,48 @@ public class Dashboard_Activity extends Activity {
                 startActivity(intent);
             }
         });
-        }
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        customerfeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard_Activity.this,CustomerFeedback_Activity.class);
+                startActivity(intent);
+            }
+        });
+        missedcustomerfeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard_Activity.this,Missed_Customer_feedback_Activity.class);
+                startActivity(intent);
+
+            }
+        });
+        employeexam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard_Activity.this,Employee_exam_Activity.class);
+                startActivity(intent);
+            }
+        });
+        result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard_Activity.this,Examresult_Activity.class);
+                startActivity(intent);
+            }
+        });
+        notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard_Activity.this,Offer_Screen_Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
 }
